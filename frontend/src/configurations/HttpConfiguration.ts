6edda61cf4221f72f37e774364084d4ad20262da
KirScr <142673005+scrsdk@users.noreply.github.com>
2025-08-15 14:47:53 +0300

    Update HttpConfiguration.ts

--- frontend/src/configurations/HttpConfiguration.ts
+++ frontend/src/configurations/HttpConfiguration.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://melvek.ru:5001";
-export const BOT_URL = "https://t.me/";
+export const BASE_URL = "https://bincoin.shop";
+export const BOT_URL = "https://t.me/Bin_CoinBot";
